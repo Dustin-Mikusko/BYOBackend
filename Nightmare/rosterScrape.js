732fe9c8abeac33bcc34b1e1ec992dbf614ceb1b
Dustin Mikusko <dustinjm713@yahoo.com>
2020-01-29 14:50:57 -0700

    Get nightmare function to scrape team site for roster

--- Nightmare/rosterScrape.js
+++ Nightmare/rosterScrape.js
@@ -1,10 +1,47 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
-import { teamURLs } from '../teams';
+import { teams } from '../teams';
 
-const getRoster = (url) => {
+// const getRoster = async (team) => {
+//   // const { urlCode, roster, id } = team;
+//   const url =`http://m.mlb.com/${urlCode}/roster/40-man/`;
+//   nightmare
+//     .goto(url)
+//     .wait(2000)
+//     .evaluate(() => {
+//       const playerDisplay = Array.from(document.querySelectorAll('tbody'));
+//       const regexPlayerData = playerDisplay.map(player => player.innerText);
+//       const sectionedPlayerData = regexPlayerData.map(player => player.split('\t'));
+//       const sectionedPlayerNames = sectionedPlayerData.map(section => section.filter(item => item.includes(' ')));
+
+//       sectionedPlayerNames.forEach(section => {
+//         section.forEach(player => {
+//           roster.push(player)
+//         });
+//       });
+//       return roster.map(player => ({
+//         firstName: player.split(' ')[0],
+//         lastName: player.split(' ')[1],
+//         teamId: id
+//       }));
+//     })
+//     .end()
+//     .then(result => result)
+//     .catch(err => console.log(err))
+// };
+
+// const getAllRosters = teams => {
+//   return teams.map(team => {
+//     getRoster(team);
+//   });
+// }
+
+// getAllRosters(teams);
+
+const getRoster = () => {
+  // const { urlCode, roster } = team;
   nightmare
-    .goto(`http://m.mlb.com/bos/roster/40-man/`)
+    .goto(`http://m.mlb.com/was/roster/40-man/`)
     .wait('.module')
     .evaluate(() => {
       const players = []
@@ -12,7 +49,7 @@ const getRoster = (url) => {
       const regexPlayerData = playerDisplay.map(player => player.innerText);
       const sectionedPlayerData = regexPlayerData.map(player => player.split('\t'));
       const sectionedPlayerNames = sectionedPlayerData.map(section => section.filter(item => item.includes(' ')));
-      
+
       sectionedPlayerNames.forEach(section => {
         section.forEach(player => {
           players.push(player)
@@ -28,29 +65,4 @@ const getRoster = (url) => {
     .catch(err => console.log(err))
 };
 
-getRoster();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// nightmare
-//   .goto('https://www.mlb.com/team')
-//   .click('a[href="https://www.mlb.com/team"]')
-//   .wait('.p-featured-content')
-//   .evaluate(() => {
-
-//   })
-//   .end()
-//   .then(result => console.log(result))
-//   .catch(err => console.log(err))
+// getRoster();
