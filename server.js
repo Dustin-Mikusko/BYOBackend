3a6912898dba867be10063bf02bd569206cd665b
Dustin Mikusko <dustinjm713@yahoo.com>
2020-01-30 11:29:20 -0700

    Add GET method for fetching single team by team id

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 app.locals.title = 'BYOBackend';
 app.use(express.json());
 
@@ -24,6 +24,23 @@ app.get('/api/v1/teams', async (req, res) => {
   } catch(err) {
     res.status(500).json({ err });
   };
+});
+
+app.get('/api/v1/teams/:id', async (req, res) => {
+  const { id } = req.params;
+  const teams = await database('teams').select();
+  const team = teams.find(team => team.id === Number(id));
+  
+  if (!team) {
+    return res.status(404).json('No team found.')
+  }
+
+  res.status(200).json({
+    id: team.id,
+    team_name: team.team_name,
+    ballpark: team.ballpark,
+    website: team.website
+  });
 });
 
 app.listen(app.get('port'), () => {
